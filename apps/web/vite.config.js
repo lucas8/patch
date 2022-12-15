78f853a3407ee918ed31380168bb0a879b5ba31e
lucas <lucas.stettner@gmail.com>
2022-12-15 12:25:48 -0500

    switch to sync

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,13 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit(), topLevelAwait(), wasm()],
-	optimizeDeps: {
-		exclude: ['@automerge/automerge-wasm']
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
