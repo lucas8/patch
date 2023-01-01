91ba9fcad948ce1743f183bc861669f919521884
lucas <lucas.stettner@gmail.com>
2023-01-01 17:18:39 -0500

    remove pwa stuff for now

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,8 +1,6 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
-declare const __DATE__: string;
-declare const __RELOAD_SW__: boolean;
 
 declare namespace App {
 	// interface Error {}
--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -6,46 +6,7 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 /** @type {import('vite').UserConfig} */
 const config = {
 	logLevel: 'info',
-	build: {
-		minify: false
-	},
-	define: {
-		__DATE__: `'${new Date().toISOString()}'`,
-		__RELOAD_SW__: false
-	},
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			srcDir: './src',
-			mode: 'development',
-			strategies: 'injectManifest',
-			filename: 'prompt-sw.ts',
-			scope: '/',
-			base: '/',
-			manifest: {
-				short_name: 'patch',
-				name: 'patch',
-				start_url: '/',
-				scope: '/',
-				display: 'standalone',
-				theme_color: '#ffffff',
-				background_color: '#ffffff',
-				icons: []
-			},
-			injectManifest: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			devOptions: {
-				enabled: true,
-				type: 'module',
-				navigateFallback: '/'
-			},
-			// if you have shared info in svelte config file put in a separate module and use it also here
-			kit: {}
-		}),
-		topLevelAwait(),
-		wasm()
-	],
+	plugins: [sveltekit(), topLevelAwait(), wasm()],
 	worker: {
 		format: 'es',
 		plugins: [wasm(), topLevelAwait()]
